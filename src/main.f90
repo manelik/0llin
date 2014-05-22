4543ae7152517a281bf81ecf5d005f2060c5ab5f
José Manuel Torres <josemanuel.tch@gmail.com>
2014-05-22 16:09:03 -0500

    Added scalar field.

--- src/main.f90
+++ src/main.f90
@@ -30,6 +30,14 @@ program main
   logical :: has_dust
 !  integer :: Nfluid
 
+! Scalar arrays
+  real(8) :: scalar_phi,scalar_pi
+  real(8) :: scalar_phi_k1,scalar_pi_k1
+
+  real(8) :: scalar_mass
+
+  logical :: has_scalar
+
 ! Space curvature
   real(8) :: k_curvature
 
@@ -44,6 +52,10 @@ program main
 ! Sign of the expansion
   integer :: exp_sign
 
+! conformal and cosmological proper time
+
+  real(8) :: t_tau, t_eta
+  real(8) :: t_tau_k1, t_eta_k1
 
 ! Output directory
 
@@ -76,7 +88,7 @@ program main
   write(*,*)"*          \/                  \/        *"
   write(*,*)"*                                        *"
   write(*,*)"*   A simple 0+1 relativistic code       *"
-  write(*,*)"*     First version: Yesterday           *"
+  write(*,*)"*     First version: 15/May/13           *"
   write(*,*)"*     Last version:  Today               *"
   write(*,*)"*                                        *"
   write(*,*)"*     Author:       Jose Manuel          *"
@@ -109,6 +121,27 @@ program main
      read(*,*) dust_rho0
   end if
 
+  write(*,*) "Is scalar field included (.false./.true.)"
+  read(*,*) has_scalar
+
+  if(has_scalar) then
+
+     write(*,*) "Scalar field mass (positive please)"
+     read(*,*) scalar_mass
+
+     if (scalar_mass<0.d0) then
+        print*, "I told you positive I'll fix that"
+        scalar_mass=-scalar_mass
+     end if
+
+     write(*,*) "Initial scalar field"
+     read(*,*) scalar_phi
+     write(*,*) "Initial time derivative ofscalar field"
+     read(*,*) scalar_pi
+
+  end if
+
+
   write(*,*) "Sign of expansion (+1 expanding, -1 contracting)"
   read(*,*) exp_sign 
 
@@ -140,10 +173,14 @@ program main
   trS = - 3.d0*cosmo_lambda/(8.d0*smallpi)
 
 ! dust
-  rho = rho + dust_rho0
+  if(has_dust) rho = rho + dust_rho0
 !  dust is pressureless, it does not add to trS
   
-
+! scalar field
+  if(has_scalar) then
+     rho = rho + half*(scalar_pi**2+(scalar_mass*scalar_phi)**2)
+     trS = trS + 3.d0*half*(scalar_pi**2-(scalar_mass*scalar_phi)**2)
+  end if
 !  rho_k1 =  cosmo_lambda/(8.d0*smallpi)
 !  trS_k1 = -3.d0*cosmo_lambda/(8.d0*smallpi)
 
@@ -157,6 +194,10 @@ program main
 
   aux = 24.d0*smallpi*rho - 9.d0*k_curvature*exp(-4.d0*phi) 
   trK = -exp_sign*sqrt(aux)
+
+! times
+  t_tau = 0.d0
+  t_eta = 0.d0
 
 ! Hamiltonian constraint, initially vanishes identically
 
@@ -180,6 +221,12 @@ program main
   write(88,*) exp(phi), "#Initial scale_factor"
   write(88,*) has_dust, "#dust_included?"
   if(has_dust)   write(88,*) dust_rho0, "#dust_initial density"
+  write(88,*) has_scalar, "#scalar_included?"
+  if(has_scalar) then
+     write(88,*) scalar_mass, "#dust_initial density"
+     write(88,*) scalar_phi, "#scalar initial value"
+     write(88,*) scalar_pi, "#scalar initial time derivative"
+  end if
   write(88,*) exp_sign, "#Sign of expansion (+1 expanding, -1 contracting)"
   write(88,*) out_dir, "#Output directory name"
 
@@ -193,6 +240,13 @@ program main
   open(unit=92,file=trim(out_dir)//"/Hubble_factor.tl",status="unknown")
 
   if(has_dust) open(unit=93,file=trim(out_dir)//"/dust_rho0.tl",status="unknown")
+  if(has_scalar) then
+     open(unit=94,file=trim(out_dir)//"/scalar_phi.tl",status="unknown")
+     open(unit=95,file=trim(out_dir)//"/scalar_pi.tl",status="unknown")
+  end if
+
+  open(unit=96,file=trim(out_dir)//"/Hubble_cal.el",status="unknown")
+
 
 ! Write starting values
 
@@ -200,15 +254,19 @@ program main
   write(89,"(2ES16.8)") t, trK
   write(90,"(2ES16.8)") t, ham
   write(91,"(2ES16.8)") t, exp(2.d0*phi)
-  write(92,"(2ES16.8)") t, -alpha*third*trK
+  write(92,"(2ES16.8)") t, -third*trK
   if(has_dust)  write(93,"(2ES16.8)") t, dust_rho0
-
+  if(has_scalar) then
+     write(94,"(2ES16.8)") t, scalar_phi 
+     write(95,"(2ES16.8)") t, scalar_pi
+  end if
+  write(96,"(2ES16.8)") t_eta, -third*trK*exp(2.d0*phi)
 ! Screen output
   write(*,*)"+-------------------------------+"
   write(*,*)"|  Time        |  Hamiltonian   |"
   write(*,*)"+-------------------------------+"
   write(*,"(2ES16.8)") t, ham
-
+  
 
 
 ! ***********************
@@ -218,10 +276,21 @@ program main
   do i=1,Nt
      !Runge-Kutta2
      ! First evaluation, midpoint estimation
+     ! Geometry
      phi_k1 = phi + half*dt*(-half*third*alpha*trK) 
      trK_k1 = trK + half*dt*(alpha*(third*trK**2+ 4.d0*smallpi*(rho+trS)))
 
+     ! Matter
      if(has_dust) dust_rho0_k1 = dust_rho0 +half*dt*( dust_rho0*alpha*trK)
+
+     if(has_scalar) then
+        scalar_phi_k1 = scalar_phi +half*dt*(alpha*scalar_pi)
+        scalar_pi_k1 = scalar_pi +half*dt*(alpha*(scalar_pi*trK-scalar_mass**2*scalar_phi))
+     end if
+
+     ! Auxiliary times
+     t_tau_k1 = t_tau +half*dt*(alpha)
+     t_eta_k1 = t_eta +half*dt*(alpha*exp(-2.d0*phi))
 
      ! Lapse for the moment is constant
      ! alpha_k1 = whatever...
@@ -232,6 +301,11 @@ program main
      trS_k1 = -3.d0*cosmo_lambda/(8.d0*smallpi)
      ! Dust
      if(has_dust) rho_k1 = rho_k1 + dust_rho0_k1
+     ! Scalar
+     if(has_scalar) then
+        rho_k1 = rho_k1 + half*(scalar_pi_k1**2+(scalar_mass*scalar_phi_k1)**2)
+        trS_k1 = trS_k1 + 3.d0*half*(scalar_pi_k1**2-(scalar_mass*scalar_phi_k1)**2)
+     end if
 
      ! Final evaluation: Full step but with sources calculated at midpoints
      phi = phi + dt*(-half*third*alpha_k1*trK_k1)
@@ -239,9 +313,16 @@ program main
 
      if(has_dust) dust_rho0 = dust_rho0 +dt*( dust_rho0_k1*alpha_k1*trK_k1)
 
+     if(has_scalar) then
+        scalar_phi = scalar_phi +dt*(alpha_k1*scalar_pi_k1)
+        scalar_pi = scalar_pi +dt*(alpha_k1*(scalar_pi_k1*trK_k1-scalar_mass**2*scalar_phi_k1))
+     end if
+
+     t_tau = t_tau +half*dt*(alpha_k1)
+     t_eta = t_eta +half*dt*(alpha_k1*exp(-2.d0*phi_k1))
+
+
      ! alpha = whatever
-     ! rho = 
-     ! trS = 
 
      t = t + dt
 
@@ -251,6 +332,11 @@ program main
      trS = -3.d0*cosmo_lambda/(8.d0*smallpi)
      ! Dust
      if(has_dust) rho = rho + dust_rho0
+     ! Scalar
+     if(has_scalar) then
+        rho = rho + half*(scalar_pi**2+(scalar_mass*scalar_phi)**2)
+        trS = trS + 3.d0*half*(scalar_pi**2-(scalar_mass*scalar_phi)**2)
+     end if
 
      ! update hamiltonian constraint
      ham = two*third*trK**2 + 6.d0*k_curvature*exp(-4.d0*phi)-16.d0*smallpi*rho
@@ -259,8 +345,13 @@ program main
      write(89,"(2ES16.8)") t, trK
      write(90,"(2ES16.8)") t, ham
      write(91,"(2ES16.8)") t, exp(2.d0*phi)
-     write(92,"(2ES16.8)") t, -alpha*third*trK
+     write(92,"(2ES16.8)") t, -third*trK
      if(has_dust) write(93,"(2ES16.8)") t, dust_rho0
+     if(has_scalar) then
+        write(94,"(2ES16.8)") t, scalar_phi 
+        write(95,"(2ES16.8)") t, scalar_pi
+     end if
+     write(96,"(2ES16.8)") t_eta, -third*trK*exp(2.d0*phi)
 
 ! Output to screen
      if(MOD(i,ft)==0) then
@@ -274,6 +365,10 @@ program main
   close(91)
   close(92)
   if(has_dust) close(93)
+  if(has_scalar) then
+     close(94)
+     close(95)
+  end if
 
   write(*,*)"+-------------------------------+"
   print*,
